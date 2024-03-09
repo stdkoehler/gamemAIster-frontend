8701411a4da7b5478ff21aecfd0e26b5d68e2ea9
Styrr <st.d.koehler@gmx.de>
2024-03-09 21:27:17 +0100

    style Textfield scollbar

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -201,7 +201,23 @@ export const StyledTextField = ({
           "& .MuiOutlinedInput-notchedOutline": {
             borderColor: darkColor, // Set default border color based on colorType
           },
-        }
+        },
+        "& .MuiInputBase-input.MuiOutlinedInput-input": {
+          '&::-webkit-scrollbar': {
+            width: '0.4em',
+            cursor: 'default !important'
+          },
+          '&::-webkit-scrollbar-track': {
+            boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+            webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,.1)',
+            outline: '1px solid',
+            color: theme.palette[color].dark,
+            cursor: "default !important"
+          }
+      }
       }}
     />
   );
