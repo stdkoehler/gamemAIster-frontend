2ec2b5835bc50c4b34d881f05d18b08744efed3c
Styrr <st.d.koehler@gmx.de>
2025-05-26 18:14:36 +0200

    remove ssl while debugging

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import fs from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('cert/key.pem'),
-      cert: fs.readFileSync('cert/cert.pem'),
-    },
-    host: true, // allows LAN access
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('cert/key.pem'),
+  //     cert: fs.readFileSync('cert/cert.pem'),
+  //   },
+  //   host: true, // allows LAN access
+  // },
 })
